18e33c6f9352c235041bf38fda80e97481c9f2c5
briochebun <bc564@scarletmail.rutgers.edu>
2021-03-08 20:53:33 -0500

    Minimal functional environment

---
+++ Assets/AgentMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentMovement : MonoBehaviour
+{
+    public Transform outsideWP, cafeteriaWP, classroomWP, bathroomWP;
+    private UnityEngine.AI.NavMeshAgent nav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // example: sets and moves the agent towards the destination
+        nav.SetDestination(classroomWP.position);
+    }
+}
